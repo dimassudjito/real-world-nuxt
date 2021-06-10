b85c472b5616a166ece5d205d5d1d378d3a89201
Dimas Sudjito <almuhtad@ualberta.ca>
2021-06-09 21:32:10 -0600

    add pages

---
+++ pages/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create an events</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can create new events in your neighbourhood',
+        },
+      ],
+    }
+  },
+}
+</script>
